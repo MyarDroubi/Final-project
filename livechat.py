06b1a3c623521717d7ce5ea31a4b16497592387b
MyarDroubi <myar.droubi@gmail.com>
2025-03-02 13:57:53 +0100

    Added livechat.py

---
+++ livechat.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random #för att skapa room-nyckel. Vi behöver inte den så tar bort i slutet. 
+from string import ascii_uppercase #för att skapa olika alphabet. Tar bort i slutet. 
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "THISISACODE"  #detta är ett lösenord som man ger. 
+socketio = SocketIO(app)
+
+
+rooms = {}
+
+def generate_unique_code(Length):
+    while True:
+        code = ""
+        for _ in range (Length):
+            code +=random.choice(ascii_uppercase)
+        
+        if code not in rooms:
+            break
+    return code
+        
+
+
+
+
+@app.route("/", methods = ["POST", "GET"])
+def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code") #kan tas bort
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter a name!" , code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room Code", code=code, name=name) #ta bort om vi inte behöver
+
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist" , code=code, name=name)
+        
+
+        session["room"] = room  #tillfälligt lagrar användaren data och vi kan manipluera ochså. 
+        session ["name"] = name
+        return redirect(url_for("room"))
+
+
+    return render_template("home.html")
+    
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    return render_template("room.html", code=room, messages = rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    content = {
+        "name" : session.get("name"),
+        "message" : data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']} ")
+
+
+@socketio.on("connect") ##vi lyssnar från script i room.html och connectar värje room till rätt room
+def connect(auth):  ##vi vet vilket room och namn dem har för att lägga dem i samma room
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    join_room(room)  #joinar room
+    send({"name": name, "message": "has entered the room"}, to=room) #skcikar meddelande till room
+    rooms[room]["members"] +=1  #hur många som finns
+    print(F"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconeect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -=1
+        if rooms[room]["members"] <=0:
+            del rooms[room]
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(F"{name} has left the room {room}")
+
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
+
+
